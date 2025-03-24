53e04c021b3af08e0d7bb6ffcc84327f7d6e9250
Sergey <berdnikausiarhei@gmail.com>
2025-03-24 16:34:42 +0300

    TMS-C32-HW-Lesson-43-44
    Spring Boot. REST

--- src/main/java/com/tms/controller/FileController.java
+++ src/main/java/com/tms/controller/FileController.java
@@ -2,13 +2,17 @@ package com.tms.controller;
 
 import com.tms.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/file")
@@ -28,4 +32,32 @@ public class FileController {
         return new ResponseEntity<>(result ? HttpStatus.CREATED : HttpStatus.CONFLICT);
     }
 
+    //Uploading multiple files
+    @GetMapping("/{filename}")
+    public ResponseEntity<Resource> getFile(@PathVariable String filename) {
+        Optional<Resource> resource = fileService.getFile(filename);
+        if (resource.isPresent()) {
+           HttpHeaders headers = new HttpHeaders();
+           headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + resource.get().getFilename());
+           return new ResponseEntity<>(resource.get(), headers, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping
+    public ResponseEntity<ArrayList<String>> getListOfFiles() {
+        ArrayList<String> files;
+        try {
+            files = fileService.getListOfFiles();
+        } catch (IOException e) {
+            System.out.println("Files not found -> " + e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        if(files.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(files, HttpStatus.OK);
+    }
 }
--- src/main/java/com/tms/service/FileService.java
+++ src/main/java/com/tms/service/FileService.java
@@ -1,18 +1,29 @@
 package com.tms.service;
 
+import org.springframework.core.io.PathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class FileService {
 
     private  final Path ROOT_PATH_FILE = Paths.get("data");
 
+    //Uploading file
     public Boolean uploadFile(MultipartFile file) {
         try {
             if (file.getOriginalFilename() == null) {
@@ -20,9 +31,25 @@ public class FileService {
             }
             Files.copy(file.getInputStream(),  ROOT_PATH_FILE.resolve(file.getOriginalFilename()));
         }catch (IOException e) {
+            System.out.println("File not found -> " + e.getMessage());
             return false;
         }
         return true;
     }
 
+    //Download
+    public Optional<Resource> getFile(String filename) {
+        Resource resource = new PathResource(ROOT_PATH_FILE.resolve(filename));
+        if (resource.exists()) {
+            return Optional.of(resource);
+        }
+        return Optional.empty();
+    }
+
+    public ArrayList<String> getListOfFiles() throws IOException {
+        return  (ArrayList<String>) Files.walk(ROOT_PATH_FILE, 1)
+                    .filter(path -> !path.equals(ROOT_PATH_FILE))
+                    .map(Path::toString)
+                    .collect(Collectors.toList());
+    }
 }
